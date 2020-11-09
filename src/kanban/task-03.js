74d88209020ba56217dc5bbae33943b653eb5d7c
Tatiana8689 <rayne18@rambler.ru>
2020-11-10 00:26:17 +0300

    Правки по ревью

--- src/kanban/task-03.js
+++ src/kanban/task-03.js
@@ -1,4 +1,4 @@
-const selectMenu = document.querySelector('.profile-menu');
+const selectMenu = document.querySelector('.shevron-icon');
 const userProfile = document.querySelector('.menu-right');
 const btn = document.querySelector('.profile');
 
@@ -7,27 +7,25 @@ list.classList.add('list');
 
 const account = document.createElement('li');
 account.innerHTML = 'My account';
+list.appendChild(account);
 account.classList.add('item');
 
 const tasks = document.createElement('li');
 tasks.innerHTML = 'My tasks';
+list.appendChild(tasks);
 tasks.classList.add('item');
 
 const exit = document.createElement('li');
 exit.innerHTML = 'Log out';
+list.appendChild(exit);
 exit.classList.add('item');
 
 btn.addEventListener('click', function () {
     userProfile.classList.toggle('open');
     
     if (userProfile.classList.contains('open')) {
-        selectMenu.style.transform ='scale(1, -1)';
         userProfile.appendChild(list);
-        list.appendChild(account);
-        list.appendChild(tasks);
-        list.appendChild(exit);
     } else {
-        selectMenu.style.transform ='scale(1, 1)';
         list.remove(); 
     }
 });
