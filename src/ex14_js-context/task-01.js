ae2ff8653751465bcb35fe04183b81301f3d471d
Tatiana8689 <rayne18@rambler.ru>
2020-11-25 23:07:00 +0300

    поправила калькулятор

--- src/ex14_js-context/task-01.js
+++ src/ex14_js-context/task-01.js
@@ -1,44 +1,44 @@
-let Calculator = {
-    initialValue: 0,
-      
-    add(a = 0) {
-      this.initialValue = this.initialValue + a;
-      
-      return this;
-    },
-  
-    subtract(a = 0) {
-      this.initialValue = this.initialValue - a;
-  
-      return this; 
-    },
-      
-    divide(a = 1) {
-      this.initialValue = this.initialValue / a;
-  
-      return this;
-    },
-      
-    multiply(a = 1) {
-      this.initialValue = this.initialValue * a;
-  
-      return this;
-    },
-      
-    getResult() { 
-      return this.initialValue;
-    },
-    
-    setState(a) {
-     this.initialValue = a;
-      
-      return this;
-    },
-    
-    reset() {
-      this.initialValue = 0;
-      
-     return this; 
-    },
+class Сalculator {
+
+  static add(a = 0) {
+    this.initialValue = this.initialValue + a;
+
+    return this;
   }
-  
+
+  static subtract(a = 0) {
+    this.initialValue = this.initialValue - a;
+
+    return this;
+  }
+
+  static divide(a = 1) {
+    this.initialValue = this.initialValue / a;
+
+    return this;
+  }
+
+  static multiply(a = 1) {
+    this.initialValue = this.initialValue * a;
+
+    return this;
+  }
+
+  static getResult() {
+    return this.initialValue;
+  }
+
+  static setState(a) {
+    this.initialValue = a;
+
+    return this;
+  }
+
+  static reset() {
+    this.initialValue = 0;
+
+    return this;
+  }
+}
+
+module.exports = Сalculator;
